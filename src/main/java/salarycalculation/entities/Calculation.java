b6fceef2b3784fffc424c4baf905dc1e16efc5bd
rgomesru <rodrigo.c.ruiz@gmail.com>
2026-06-10 11:50:13 -0300

    Atualização de Exercícios.

--- src/main/java/salarycalculation/entities/Calculation.java
+++ src/main/java/salarycalculation/entities/Calculation.java
@@ -11,7 +11,7 @@ public class Calculation {
     }
 
     public double netSalaryWithPromotion() {
-        return netSalary() + (promotion / 100 * netSalary());
+        return grossSalary + (promotion / 100 * grossSalary);
     }
 
 }
